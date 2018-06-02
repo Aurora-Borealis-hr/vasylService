2e17e752f873d27bb2e48d8a4adeff917e4f9f2a
vasyl-n <vasyl.nrk@gmail.com>
2018-06-02 15:25:36 -0700

    adds tag route

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
  
 app.get('/', (req, res) => {
-  console.log(process.env.NODE_ENV)
   res.send('Hello world');
 });
 
-app.post('/new-user', (req, res) => {
-  db.saveVideo(req.body, () => res.sendStatus(201));
+app.post('/videos', (req, res) => {
+  db.newVideo(req.body, () => res.sendStatus(201));
+})
+
+app.post('/tags', (req, res) => {
+  db.newTag(req.body, () => res.sendStatus(201));
 })
 
 module.exports = app.listen(process.env.PORT || process.env.NODE_ENV === 'test' ? 3001 : 3000);
--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,34 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/videoService')
-const Video = mongoose.model('Video', {name: String});
+const Video = mongoose.model('Video', {
+  name: String,
+  description: String,
+  duration: Number,
+  link: String,
+});
+
+const Tag = mongoose.model('Tag', {
+  name: String,
+
+});
+
+const Chanel = mongoose.model('Chanel', {
+  name: String,
+  owner: Number,
+});
 
 const saveVideo = (obj, callback) => {
   const video = new Video(obj);
   video.save().then(() => callback());
 }
 
+const newTag = (obj, callback) => {
+  const tag = new Tag(obj);
+  tag.save().then(() => callback());
+}
+
 module.exports = {
   saveVideo,
+  newTag,
 }
