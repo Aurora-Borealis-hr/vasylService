432ae01bf0d8b5ec897daaebaa4c0f30e9ef267e
vasyl-n <vasyl.nrk@gmail.com>
2018-06-05 19:54:57 -0700

    adds add tag route

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.post('/videos', (req, res) => {
   db.newVideo(req.body, () => res.sendStatus(201));
 })
 
-app.post('/tags', (req, res) => {
-  db.newTag(req.body, () => res.sendStatus(201));
+app.post('/tags/:tag', (req, res) => {
+  db.newTag({name: req.params.tag, numOfVideos: 0}, () => res.sendStatus(201));
 })
 
 module.exports = app.listen(process.env.PORT || process.env.NODE_ENV === 'test' ? 3001 : 3000);
