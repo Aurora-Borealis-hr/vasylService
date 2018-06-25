ab385d2e4c792e9be600e2da6d63335df2b45c84
vasyl-n <vasyl.nrk@gmail.com>
2018-06-24 23:08:19 -0700

    adds get tag and get tags helpers

--- db/utils/tag.js
+++ db/utils/tag.js
@@ -1,4 +1,5 @@
 const Tag = require('../models/tag').Tag
+const knex = require('../server.js').knex
 
 const newTag = (obj, callback) => {
   Tag.sync().then(() => {
@@ -12,4 +13,18 @@ const newTag = (obj, callback) => {
   });
 }
 
+const getTag = (id, callback) => {
+  return new Promise((resolve) => {
+    knex('tags').where({
+      id: id
+    }).select().then((data) => resolve(data))
+  })
+}
+
+const getTags = (ids, callback) => {
+    knex('tags').whereIn('id', ids).select('name').then((data) => callback(data))
+}
+
 module.exports.newTag = newTag;
+module.exports.getTag = getTag;
+module.exports.getTags = getTags;
